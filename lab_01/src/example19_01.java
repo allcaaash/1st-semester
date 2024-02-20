d96f8be6c5ed5194d74e6495d0a33fd6ab74d5b0
all_caaash <une066@gmail.com>
2024-02-20 23:13:47 +0500

    lab_01 added

---
+++ lab_01/src/example19_01.java
@@ -0,0 +1,8 @@
+package lab01;
+
+public class example19_01 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
---
+++ lab_01/src/example19_03.java
@@ -0,0 +1,20 @@
+package lab01;
+
+public class example19_03 {
+    public static void main(String[] args) {
+        int s, d;
+        s = 10;
+        d = 20;
+        if (s < d){
+            System.out.println("S < D");
+        }
+        s = s *2;
+        if(s == d){
+            System.out.println("S = D");
+        }
+        s = s * d;
+        if (s > d){
+            System.out.println("S > D");
+        }
+    }
+}
